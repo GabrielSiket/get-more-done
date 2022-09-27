9ff8adf90dbe4599faf153df2c112c3218ec3ed8
Gabriel S <gabriel.siket@gmail.com>
2022-09-27 16:35:30 +0000

    add Edit queryset item option

--- get_more_done/views.py
+++ get_more_done/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib import messages
 from get_more_done.models import TaskList
 from get_more_done.forms import TaskForm
-from django.contrib import messages
+
+
 
 def todolist(request):
     if request.method == "POST":
@@ -13,14 +15,27 @@ def todolist(request):
         return redirect('todolist')
     else:
         all_tasks = TaskList.objects.all
-    
         return render(request, 'todolist.html', {'all_tasks': all_tasks})
+
 
 def delete_task(request, task_id):
     task = TaskList.objects.get(pk=task_id)
     task.delete()
-    
     return redirect('todolist')
+
+def edit_task(request, task_id):
+    if request.method == "POST":
+        task = TaskList.objects.get(pk=task_id)
+        form = TaskForm(request.POST or None, instance = task)
+        if form.is_valid():
+            form.save()
+
+        messages.success(request, ("Task Edited!"))
+        return redirect('todolist')
+    else:
+        task_obj = TaskList.objects.get(pk=task_id)
+        return render(request, 'edit.html', {'task_obj': task_obj})
+
 
 def contact(request):
     context = {
@@ -28,6 +43,7 @@ def contact(request):
 
     }
     return render(request, 'contact.html', context)
+
 
 def about(request):
     context = {
