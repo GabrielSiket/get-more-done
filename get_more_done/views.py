25a414606726608ac99816117941ca9d3ebbb350
Gabriel S <gabriel.siket@gmail.com>
2022-09-27 17:43:25 +0000

    add pagination

--- get_more_done/views.py
+++ get_more_done/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib import messages
+from django.core.paginator import Paginator
 from get_more_done.models import TaskList
 from get_more_done.forms import TaskForm
 
@@ -14,7 +15,10 @@ def todolist(request):
         messages.success(request, ("New Task Added!"))
         return redirect('todolist')
     else:
-        all_tasks = TaskList.objects.all
+        all_tasks = TaskList.objects.all()
+        paginator = Paginator(all_tasks, 5)
+        page = request.GET.get('pg')
+        all_tasks = paginator.get_page(page)
         return render(request, 'todolist.html', {'all_tasks': all_tasks})
 
 
