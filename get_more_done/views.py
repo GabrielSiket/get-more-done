800ba0b1479b8b562576d76d0b44e832b43efd46
Gabriel S <gabriel.siket@gmail.com>
2022-10-03 13:02:25 +0000

    add login & logout functionality and add page restrictions

--- get_more_done/views.py
+++ get_more_done/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
 from django.contrib import messages
 from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
 from get_more_done.models import TaskList
 from get_more_done.forms import TaskForm
 
-
-
+@login_required
 def todolist(request):
     if request.method == "POST":
         form = TaskForm(request.POST or None)
@@ -21,12 +20,13 @@ def todolist(request):
         all_tasks = paginator.get_page(page)
         return render(request, 'todolist.html', {'all_tasks': all_tasks})
 
-
+@login_required()
 def delete_task(request, task_id):
     task = TaskList.objects.get(pk=task_id)
     task.delete()
     return redirect('todolist')
 
+@login_required()
 def edit_task(request, task_id):
     if request.method == "POST":
         task = TaskList.objects.get(pk=task_id)
@@ -40,18 +40,21 @@ def edit_task(request, task_id):
         task_obj = TaskList.objects.get(pk=task_id)
         return render(request, 'edit.html', {'task_obj': task_obj})
 
+@login_required()
 def complete_task(request, task_id):
     task = TaskList.objects.get(pk=task_id)
     task.done = True
     task.save()
     return redirect('todolist')
 
+@login_required()
 def pending_task(request, task_id):
     task = TaskList.objects.get(pk=task_id)
     task.done = False
     task.save()
     return redirect('todolist')
 
+@login_required()
 def index(request):
     context = {
         'index_text': "Welcome to Index Page !",
