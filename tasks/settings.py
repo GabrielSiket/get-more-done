9978374fc2a32b6d90edf5d86e86c5ec265dc888
Gabriel S <gabriel.siket@gmail.com>
2022-10-11 12:24:00 +0000

    Initiate heroku deployment

--- tasks/settings.py
+++ tasks/settings.py
@@ -1,3 +1,4 @@
+import django_heroku
 import os
 import environ
 
@@ -121,3 +122,4 @@ LOGIN_REDIRECT_URL = "todolist"
 LOGIN_URL = "login"
 
 # Heroku local settings
+django_heroku.settings(locals())
